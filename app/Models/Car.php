7b6a71b7dedba26a2814ef22898eef0cdf050516
Ari Jay <ikuskid7@gmail.com>
2022-03-03 10:47:38 +0100

    add geographical trait to users and car models

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use App\Models\Traits\Geographical;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Car extends Model
 {
-    use HasFactory;
+    use HasFactory, Geographical;
 
     /**
      * The attributes that are mass assignable.
